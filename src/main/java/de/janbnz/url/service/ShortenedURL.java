f5cce44c795890a414e0e9f52fd98e8f5b948db5
Jan <janbenz2907@gmail.com>
2023-06-20 19:07:30 +0200

    Change ShortenedURL class to record

--- src/main/java/de/janbnz/url/service/ShortenedURL.java
+++ src/main/java/de/janbnz/url/service/ShortenedURL.java
@@ -1,25 +1,11 @@
 package de.janbnz.url.service;
 
-public class ShortenedURL {
-
-    private final String originalURL, shortenedURL;
-    private final int redirects;
-
-    public ShortenedURL(String originalURL, String shortenedURL, int redirects) {
-        this.originalURL = originalURL;
-        this.shortenedURL = shortenedURL;
-        this.redirects = redirects;
-    }
-
-    public int getRedirects() {
-        return redirects;
-    }
-
+public record ShortenedURL(String originalURL, String shortenedURL, int redirects) {
     public String getOriginalURL() {
         return originalURL;
     }
 
-    public String getShortenedURL() {
-        return shortenedURL;
+    public int getRedirects() {
+        return redirects;
     }
 }
